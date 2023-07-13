896838455171e4bc2683b12c1eb76789bb470c60
Shreyas Deshpande <shreyaspractice1@gmail.com>
2023-07-13 07:52:05 +0530

    #first initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { DemoComponent } from './demo/demo.component';
+import { Demo1Component } from './demo1/demo1.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DemoComponent,
+    Demo1Component,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
